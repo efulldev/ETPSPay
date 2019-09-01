f823ba6dd7eb4141c537137832168cc9ded6492a
@zheeno <efezinoukpowe@gmail.com>
2019-09-01 23:17:04 +0100

    modified mainActivity to support settings and sign out

--- app/src/main/java/com/efulltech/etpspay/ui/MainActivity.java
+++ app/src/main/java/com/efulltech/etpspay/ui/MainActivity.java
@@ -47,60 +47,23 @@ public class MainActivity extends AppCompatActivity {
     Button transactionHistory;
 //    @BindView(R.id.printEODBtn)
 //    Button printReceipt;
-//    @BindView(R.id.userPreferencesBtn)
-//    Button userPreferences;
+    @BindView(R.id.userPreferencesBtn)
+    Button userPreferences;
 //
-//    DataProccessor dataProccessor;
+    DataProccessor dataProccessor;
 //
 //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        timeText = findViewById(R.id.timeText);
-//        dateText = findViewById
-
-        //get current device seconds (Time)
-        @SuppressLint("SimpleDateFormat") SimpleDateFormat curSec = new SimpleDateFormat("ss");
-        // convert seconds to int
-        Integer timer = 60 - Integer.parseInt(curSec.format(new Date()));
-        Log.d(TAG, "Timer: " + timer);
-        // start the recursive function to display time
-        showDateTime(timer);
-
         ButterKnife.bind(this);
-//
-//        dataProccessor = new DataProccessor(this);
+        dataProccessor = new DataProccessor(this);
 //
 //        checkOsVersion();
 
     }
 
-    private void showDateTime(int _secTimer) {
-        final Integer secTimer = _secTimer;
-        @SuppressLint("SimpleDateFormat") SimpleDateFormat curTime = new SimpleDateFormat("HH:mm:ss z");
-        @SuppressLint("SimpleDateFormat") SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd G");
-        String currentTime = curTime.format(new Date());
-//        timeText.setText(currentTime);
-
-        Thread background = new Thread() {
-            public void run() {
-                try {
-                    // Thread will sleep for 'secTimer' seconds
-                    sleep(secTimer * 1000);
-
-                    // After 5 seconds call showDateTime
-                    showDateTime(secTimer);
-                }
-                catch (Exception e) {
-                    Log.d("Main", e.toString());
-                }
-            }
-        };
-        // start thread
-        background.start();
-    }
 
     //
     @OnClick(R.id.cardPaymentBtn)
@@ -108,20 +71,20 @@ public class MainActivity extends AppCompatActivity {
         Intent cardPayment = new Intent(MainActivity.this, CardPaymentActivity.class);
         startActivity(cardPayment);
     }
-//
-//    @OnClick(R.id.printEODBtn2)
-//    public void logOut(View view){
-//
-//        LoginDataSource dataSource = new LoginDataSource();
-//        LoginRepository loginRepository = LoginRepository.getInstance(dataSource);
-//        loginRepository.logout();
-//
-//        finish();
-//        // open slash screen activity
-//        Intent splashIntent = new Intent(MainActivity.this, SplashActivity.class);
-//        startActivity(splashIntent);
-//
-//    }
+
+    @OnClick(R.id.signOutBtn)
+    public void logOut(View view){
+
+        LoginDataSource dataSource = new LoginDataSource();
+        LoginRepository loginRepository = LoginRepository.getInstance(dataSource);
+        loginRepository.logout();
+
+        finish();
+        // open slash screen activity
+        Intent splashIntent = new Intent(MainActivity.this, SplashActivity.class);
+        startActivity(splashIntent);
+
+    }
 
 //
     @OnClick(R.id.transactionHistoryBtn)
@@ -139,65 +102,65 @@ public class MainActivity extends AppCompatActivity {
 //        usbPrinter.PrintDemoText();
 //    }
 //
-//    @OnClick(R.id.userPreferencesBtn)
-//    protected void userPreferences(View view) {
-//        confirmPinThenSettings();
+    @OnClick(R.id.userPreferencesBtn)
+    public void userPreferences(View view) {
+        confirmPinThenSettings();
+
+    }
+
+    private void confirmPinThenSettings() {
+
+        // get prompts.xml view
+        LayoutInflater layoutInflater = LayoutInflater.from(MainActivity.this);
+        View promptView = layoutInflater.inflate(R.layout.admin_pin_dialog, null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+        alertDialogBuilder.setView(promptView);
+
+        final EditText uPin = promptView.findViewById(R.id.pinTextField);
+        dataProccessor.setStr(Constants.OPERATOR_PIN, "1234");
+        dataProccessor.setStr(Constants.SUPERVISOR_PIN, "1111");
+        dataProccessor.setStr(Constants.ADMIN_PIN, "260089");
+
+        // setup a dialog window
+        alertDialogBuilder.setCancelable(false)
+                .setPositiveButton("Confirm", (dialog, id) -> {
+
+                    String pin = uPin.getText().toString();
+                    String permission = "";
+                    if(dataProccessor.getStr(Constants.OPERATOR_PIN).equals(pin) ||
+                            dataProccessor.getStr(Constants.SUPERVISOR_PIN).equals(pin) ||
+                            dataProccessor.getStr(Constants.ADMIN_PIN).equals(pin)){
+                        if(Constants.ADMIN_PIN.equals(pin)){
+                            dataProccessor.setStr(Constants.ADMIN, "admin");
+//                            Log.d("MainActivity", "Admin:" + pin);
+//                            permission = "admin";
+                        }else if(Constants.SUPERVISOR_PIN.equals(pin)){
+                            dataProccessor.setStr(Constants.SUPERVISOR, "supervisor");
+//                            Log.d("MainActivity", "Supervisor:" + pin);
+//                            permission = "supervisor";
+                        }else{
+                            dataProccessor.setStr(Constants.OPERATOR, "operator");
+//                            Log.d("MainActivity", "Operator:" + pin);
+//                            permission = "operator";
+                        }
+//                            display SettingsActivity page
+                        Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
 //
-//    }
-//
-//    private void confirmPinThenSettings() {
-//
-//        // get prompts.xml view
-//        LayoutInflater layoutInflater = LayoutInflater.from(MainActivity.this);
-//        View promptView = layoutInflater.inflate(R.layout.admin_pin_dialog, null);
-//        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
-//        alertDialogBuilder.setView(promptView);
-//
-//        final EditText uPin = promptView.findViewById(R.id.pinTextField);
-//        dataProccessor.setStr(Constants.OPERATOR_PIN, "1234");
-//        dataProccessor.setStr(Constants.SUPERVISOR_PIN, "1111");
-//        dataProccessor.setStr(Constants.ADMIN_PIN, "260089");
-//
-//        // setup a dialog window
-//        alertDialogBuilder.setCancelable(false)
-//                .setPositiveButton("Confirm", (dialog, id) -> {
-//
-//                    String pin = uPin.getText().toString();
-//                    String permission = "";
-//                    if(dataProccessor.getStr(Constants.OPERATOR_PIN).equals(pin) ||
-//                            dataProccessor.getStr(Constants.SUPERVISOR_PIN).equals(pin) ||
-//                            dataProccessor.getStr(Constants.ADMIN_PIN).equals(pin)){
-//                        if(Constants.ADMIN_PIN.equals(pin)){
-//                            dataProccessor.setStr(Constants.ADMIN, "admin");
-////                            Log.d("MainActivity", "Admin:" + pin);
-////                            permission = "admin";
-//                        }else if(Constants.SUPERVISOR_PIN.equals(pin)){
-//                            dataProccessor.setStr(Constants.SUPERVISOR, "supervisor");
-////                            Log.d("MainActivity", "Supervisor:" + pin);
-////                            permission = "supervisor";
-//                        }else{
-//                            dataProccessor.setStr(Constants.OPERATOR, "operator");
-////                            Log.d("MainActivity", "Operator:" + pin);
-////                            permission = "operator";
-//                        }
-////                            display SettingsActivity page
-//                        Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-////
-//                        startActivity(intent);
-//                    }else{
-////                            alert invalid PIN
-//                        Snackbar.make(findViewById(android.R.id.content), "Invalid PIN! Please try again", Snackbar.LENGTH_LONG).show();
-//                    }
-//                })
-//                .setNegativeButton("Cancel",
-//                        (dialog, id) ->
-//                                dialog.cancel());
-//
-//        // create an alert dialog
-//        AlertDialog alert = alertDialogBuilder.create();
-//        alert.show();
-//
-//    }
+                        startActivity(intent);
+                    }else{
+//                            alert invalid PIN
+                        Snackbar.make(findViewById(android.R.id.content), "Invalid PIN! Please try again", Snackbar.LENGTH_LONG).show();
+                    }
+                })
+                .setNegativeButton("Cancel",
+                        (dialog, id) ->
+                                dialog.cancel());
+
+        // create an alert dialog
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+
+    }
 //
 //    public void checkOsVersion() {
 //
