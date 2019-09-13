383074ca4e992ac3e5e69356d5c850f488702813
unknown <orafu@gmail.com>
2019-09-13 11:16:27 +0100

    changes

--- app/src/main/java/com/efulltech/etpspay/ui/auth/login/LoginActivity.java
+++ app/src/main/java/com/efulltech/etpspay/ui/auth/login/LoginActivity.java
@@ -134,7 +134,7 @@ public class LoginActivity extends AppCompatActivity implements TextToSpeech.OnI
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loadingProgressBar.setVisibility(View.VISIBLE);
+                 loadingProgressBar.setVisibility(View.VISIBLE);
                 loginViewModel.login(usernameEditText.getText().toString(),
                         passwordEditText.getText().toString());
             }
